3a24b3a6ef8f3e324f4d9d88da72851a379d667a
Aris Buzachis <buzachis.aris@gmail.com>
2023-09-29 17:08:29 +0300

    Update book-list.component.tsx

--- components/react-books-app/src/components/book-list.component.tsx
+++ components/react-books-app/src/components/book-list.component.tsx
@@ -1,4 +1,4 @@
-//zzzzzzzzzzzzzzzzzzzzzzzzzzqqzzqqzzqwezzzzz
+//zzzzzzzzzzzzzzzzzzzzzzzzzzqqzzqqzzqwezzzzzz
 import { Component, ChangeEvent } from "react";
 import BookDataService from "../services/book.service";
 import { Link } from "react-router-dom";
